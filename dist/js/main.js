6f17da8d957b6f27adfe855e80bc67977aceeb8d
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-02-11 00:50:01 +0700

    deploy

--- dist/js/main.js
+++ dist/js/main.js
@@ -44,5 +44,110 @@
       document.body.classList.remove("no-scroll")
     });
 
+    $('.liveline__slider').slick({
+      infinite: true,
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false
+    });
+
+    $('.reviews__slider').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M8 2L2 8L8 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M2 2L8 8L2 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+
+      responsive: [
+        {
+          breakpoint: 1389,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            slidesToShow: 3,
+            variableWidth: true
+          }
+        }
+      ]
+    });
+
+    $('.rec__slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipeToSlide: true,
+      centerMode: true,
+      variableWidth: true,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M8 2L2 8L8 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M2 2L8 8L2 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
+    });
+
+    $(function () {
+      $('.liveline__stat').each(function () {
+        $(this).prop('Counter', 0).animate({
+          Counter: $(this).text()
+        }, {
+          duration: 1500,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      });
+    });
+
+    $(function () {
+      $('.stat__title').each(function () {
+        $(this).prop('Counter', 0).animate({
+          Counter: $(this).text()
+        }, {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      });
+    });
+
+    (function () {
+      var close = document.querySelectorAll('.modal__close-btn');
+      var openBtn = document.querySelectorAll('.hero__btn');
+
+      Array.from(openBtn, openButton => {
+        openButton.addEventListener('click', e => {
+          let modalId = e.target.getAttribute('data-id');
+          document.getElementById(modalId).classList.add("open");
+
+          Array.from(close, closeButton => {
+            closeButton.addEventListener('click', e => document.getElementById(modalId).classList.remove("open"));
+
+            window.addEventListener('keydown', (e) => {
+              if (e.key === "Escape") {
+                document.getElementById(modalId).classList.remove("open")
+              }
+            });
+
+            document.querySelector(".modal.open .modal__box").addEventListener('click', event => {
+              event._isClickWithInModal = true;
+            });
+
+            document.getElementById(modalId).addEventListener('click', event => {
+              if (event._isClickWithInModal) return;
+              event.currentTarget.classList.remove('open');
+            });
+          });
+        });
+      });
+    })();
+
   });
 })();

f1df1a06e48ac6348693c139cc62e790e9e839ba
AlexandrWalker <alexandr.d.walker@gmail.com>
2024-02-11 16:35:30 +0700

    main page done

--- app/js/main.js
+++ app/js/main.js
@@ -51,7 +51,28 @@
       swipeToSlide: true,
       autoplay: true,
       autoplaySpeed: 2000,
-      arrows: false
+      arrows: false,
+
+      responsive: [
+        {
+          breakpoint: 1389,
+          settings: {
+            slidesToShow: 6,
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+          }
+        },
+        {
+          breakpoint: 680,
+          settings: {
+            slidesToShow: 3,
+          }
+        }
+      ]
     });
 
     $('.reviews__slider').slick({
@@ -71,7 +92,8 @@
             arrows: false,
             centerMode: true,
             slidesToShow: 3,
-            variableWidth: true
+            variableWidth: true,
+            dots: true,
           }
         }
       ]
@@ -85,9 +107,20 @@
       centerMode: true,
       variableWidth: true,
       autoplay: false,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 1000,
       prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M8 2L2 8L8 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
-      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M2 2L8 8L2 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>'
+      nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="14" viewBox="0 0 10 16" fill="none"><path d="M2 2L8 8L2 14" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+
+      responsive: [
+        {
+          breakpoint: 999,
+          settings: {
+            arrows: false,
+            slidesToShow: 3,
+            dots: true,
+          }
+        }
+      ]
     });
 
     $(function () {
@@ -120,19 +153,42 @@
 
     (function () {
       var close = document.querySelectorAll('.modal__close-btn');
-      var openBtn = document.querySelectorAll('.hero__btn');
+      var openBtn = document.querySelectorAll('.modal-btn');
 
       Array.from(openBtn, openButton => {
         openButton.addEventListener('click', e => {
           let modalId = e.target.getAttribute('data-id');
+
+          document.getElementById("modal-video-1").setAttribute('src', 'https://www.youtube.com/embed/jBCwxv8Ibio');
+          document.getElementById("rec-video-1").setAttribute('src', 'https://www.youtube.com/embed/h-2OJAU92b0?start=844');
+          document.getElementById("rec-video-2").setAttribute('src', 'https://www.youtube.com/embed/oSynyHJyWnA?start=251');
+          document.getElementById("rec-video-3").setAttribute('src', 'https://www.youtube.com/embed/u5WAXkUXhzQ?start=404');
+          document.getElementById("rec-video-4").setAttribute('src', 'https://www.youtube.com/embed/EbYONp3hv_0?start=59');
+          document.getElementById("rec-video-5").setAttribute('src', 'https://www.youtube.com/embed/L4nj-G-ybQc?start=143');
+
           document.getElementById(modalId).classList.add("open");
 
           Array.from(close, closeButton => {
-            closeButton.addEventListener('click', e => document.getElementById(modalId).classList.remove("open"));
+            closeButton.addEventListener('click', e => {
+              document.getElementById(modalId).classList.remove("open");
+
+              document.getElementById("modal-video-1").setAttribute('src', '');
+              document.getElementById("rec-video-1").setAttribute('src', '');
+              document.getElementById("rec-video-2").setAttribute('src', '');
+              document.getElementById("rec-video-3").setAttribute('src', '');
+              document.getElementById("rec-video-4").setAttribute('src', '');
+              document.getElementById("rec-video-5").setAttribute('src', '');
+            });
 
             window.addEventListener('keydown', (e) => {
               if (e.key === "Escape") {
-                document.getElementById(modalId).classList.remove("open")
+                document.getElementById(modalId).classList.remove("open");
+                document.getElementById("modal-video-1").setAttribute('src', '');
+                document.getElementById("rec-video-1").setAttribute('src', '');
+                document.getElementById("rec-video-2").setAttribute('src', '');
+                document.getElementById("rec-video-3").setAttribute('src', '');
+                document.getElementById("rec-video-4").setAttribute('src', '');
+                document.getElementById("rec-video-5").setAttribute('src', '');
               }
             });
 
@@ -143,6 +199,12 @@
             document.getElementById(modalId).addEventListener('click', event => {
               if (event._isClickWithInModal) return;
               event.currentTarget.classList.remove('open');
+              document.getElementById("modal-video-1").setAttribute('src', '');
+              document.getElementById("rec-video-1").setAttribute('src', '');
+              document.getElementById("rec-video-2").setAttribute('src', '');
+              document.getElementById("rec-video-3").setAttribute('src', '');
+              document.getElementById("rec-video-4").setAttribute('src', '');
+              document.getElementById("rec-video-5").setAttribute('src', '');
             });
           });
         });
